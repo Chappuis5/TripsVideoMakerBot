0cdda456035bc3ee65715ae6a35de2c73a51698a
Chappuis5 <evancflament@gmail.com>
2022-11-14 16:58:07 +0100

    feat: toml config file

--- videocreation/subtitles.py
+++ videocreation/subtitles.py
@@ -19,10 +19,10 @@ def second_to_timecode(x: float) -> str:
 
     return '%.2d:%.2d:%.2d,%.3d' % (hour, minute, second, millisecond)
 
-def to_srt(
-        words: Sequence[pvleopard.Leopard.Word],
-        endpoint_sec: float = 1.,
-        length_limit: Optional[int] = 1) -> str:
+def to_srt(length_limit, endpoint_sec,
+        words: Sequence[pvleopard.Leopard.Word]) -> str:
+        #endpoint_sec: float = 0.5) -> str:
+        #length_limit: Optional[int] = 4) -> str:
     def _helper(end: int) -> None:
         lines.append("%d" % section)
         lines.append(
@@ -49,7 +49,8 @@ def to_srt(
     return '\n'.join(lines)
 
 
-def subMaker(story, country):
+
+def subMakerLower(story, country, bold, italic, font, font_size, pm_transp, out_transp, border_style, margin, length_limit, endpoint_sec):
     leopard = pvleopard.create(access_key="JHRxxr3akK4RilsSIOyULG8IMwwmbMQX6fLcTeB2yXgXSsDWcexbdA==")
 
     file = "final.mp3"
@@ -63,8 +64,70 @@ def subMaker(story, country):
     parent_dir1 = "./assets/subtitles/"
     file1 = "subtitles.srt"
     with open(os.path.join(parent_dir1, file1), 'w') as f:
-        f.write(to_srt(words)) 
+        f.write(to_srt(length_limit, endpoint_sec, words)) 
+    
+    dir = "subtitles"
+    path = "./assets/"
+    full_path2 = os.path.join(path, dir)
 
+    ## To lower case ##
+    for filename in os.listdir(full_path2):
+        if filename.endswith(".srt"):
+            os.rename(os.path.join(full_path2, filename), os.path.join(full_path2, filename.replace(".srt", ".txt")))
+            
+    txt_file = open(os.path.join(full_path2, filename.replace(".srt", ".txt")), 'r')
+    for x in txt_file.read():
+        y = x.lower()
+        txt_file_upper = open(os.path.join(full_path2, "subtitles_down.txt"), 'a')
+        txt_file_upper.write(y)
+        txt_file_upper.close()
+    txt_file.close()
+    os.remove(os.path.join(full_path2, filename.replace(".srt", ".txt")))
+
+
+    for name in os.listdir(full_path2):
+        if name.endswith(".txt"):
+            os.rename(os.path.join(full_path2, name), os.path.join(full_path2, name.replace(".txt", ".srt")))
+
+    semi_final_video = "final_clip.mp4"
+    parent_dir2 = "./assets/final/"
+    full_path3 = os.path.join(parent_dir2, semi_final_video)
+    full_path_test = "./assets/final/final_clip.mp4"
+    #up_sub = "subtitles_up.srt"
+    down_sub = "subtitles_down.srt"
+    parent_dir3 =  "assets/subtitles/"
+    full_path4 = os.path.join(parent_dir3, down_sub)
+
+    video = ffmpeg.input(full_path3)
+    audio = video.audio
+    video_index = 0
+    final_video_path = f"./output/{country}/final_{story}_0.mp4"
+    while(os.path.exists(final_video_path)):
+        video_index += 1
+        final_video_path = f"./output/{country}/final_{story}_{video_index}.mp4"
+       
+    ffmpeg.concat(video.filter("subtitles", full_path4, force_style=f'Fontsize={font_size},Fontname={font},PrimaryColour=&H{pm_transp}FFFFFF,Italic={italic},Bold={bold},Outline=1,OutlineColour=&H{out_transp}000000,BorderStyle={border_style},Alignment=2,MarginV={margin}'), audio, v=1, a=1).output(final_video_path).run() 
+
+    #ffmpeg.concat(video.filter("subtitles", full_path4, force_style='Fontsize=10,Fontname=Futura,PrimaryColour=&H00FFFFFF,Italic=1,Bold=1,Outline=1,OutlineColour=&H00000000,BorderStyle=2,Alignment=2,MarginV=145'), audio, v=1, a=1).output(final_video_path).run()     
+
+def subMaker(story, country, bold, italic, font, font_size, pm_transp, out_transp, border_style, margin, length_limit, endpoint_sec):
+    leopard = pvleopard.create(access_key="JHRxxr3akK4RilsSIOyULG8IMwwmbMQX6fLcTeB2yXgXSsDWcexbdA==")
+
+    file = "final.mp3"
+    parent_dir = "./assets/finalAudio/"
+    full_path = os.path.join(parent_dir, file)
+    transcript, words = leopard.process_file(full_path)
+
+
+    ### CREATE SUBTITLES.SRT
+    mkDir("./assets/subtitles")
+    parent_dir1 = "./assets/subtitles/"
+    file1 = "subtitles.srt"
+    with open(os.path.join(parent_dir1, file1), 'w') as f:
+        f.write(to_srt(length_limit, endpoint_sec, words)) 
+    
+
+    
     ### SUBTITLES.SRT TO UPPERCASE
     dir = "subtitles"
     path = "./assets/"
@@ -80,8 +143,9 @@ def subMaker(story, country):
         txt_file_upper = open(os.path.join(full_path2, "subtitles_up.txt"), 'a')
         txt_file_upper.write(y)
         txt_file_upper.close()
-    os.remove(os.path.join(full_path2, filename.replace(".srt", ".txt")))
     txt_file.close()
+    os.remove(os.path.join(full_path2, filename.replace(".srt", ".txt")))
+    
 
     for name in os.listdir(full_path2):
         if name.endswith(".txt"):
@@ -102,6 +166,7 @@ def subMaker(story, country):
     while(os.path.exists(final_video_path)):
         video_index += 1
         final_video_path = f"./output/{country}/final_{story}_{video_index}.mp4"
-       
-    ffmpeg.concat(video.filter("subtitles", full_path4, force_style='Fontsize=20,Fontname=LEMON MILK,PrimaryColour=&H00FFFFFF,Italic=0,Bold=1,Outline=2,OutlineColour=&H00000000,BorderStyle=3,Alignment=2,MarginV=145'), audio, v=1, a=1).output(final_video_path).run()   
     
+    ffmpeg.concat(video.filter("subtitles", full_path4, force_style=f'Fontsize={font_size},Fontname={font},PrimaryColour=&H{pm_transp}FFFFFF,Italic={italic},Bold={bold},Outline=1,OutlineColour=&H{out_transp}000000,BorderStyle={border_style},Alignment=2,MarginV={margin}'), audio, v=1, a=1).output(final_video_path).run() 
+
+    #ffmpeg.concat(video.filter("subtitles", full_path4, force_style='Fontsize=10,Fontname=Futura,PrimaryColour=&H00FFFFFF,Italic=1,Bold=1,Outline=1,OutlineColour=&H00000000,BorderStyle=2,Alignment=2,MarginV=145'), audio, v=1, a=1).output(final_video_path).run()     
